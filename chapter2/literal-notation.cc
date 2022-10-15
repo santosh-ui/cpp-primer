0a90137bde7166becce118375ca25c33711c2661
santosh-ui <rajusantosh77@gmail.com>
2022-10-15 15:35:07 -0400

    adds literal notation example

---
+++ chapter2/literal-notation.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+	float d = 1024;
+	std::cout << d << std::endl;
+	std::cout << "who goes with F\145rgus? \012" << std::endl;
+	std::cout << "2\t\115\12" << std::endl;
+
+	return 0;
+}
