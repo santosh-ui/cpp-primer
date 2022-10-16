464b5d77b87c4dd0dbf07ee9135f950bd21f1984
santosh-ui <rajusantosh77@gmail.com>
2022-10-16 14:35:13 -0400

    adds variable initialization examples

--- chapter2/variables.cc
+++ chapter2/variables.cc
@@ -1,6 +1,6 @@
 #include<iostream>
-std::string global_str;
-int global_int;
+// std::string global_str;
+// int global_int;
 int main(){
 //	int input_value = 0;
 //	int i = 3.14; // no error double converted to int
@@ -10,8 +10,13 @@ int main(){
 //	std::cin >> input_value;
 //	std::cout << "the input value is: " << input_value << "\ti value is: " << i << std::endl;
 	
-	std::string local_str;
-	int local_int;
-	std::cout << "/ " << local_str << " /" << "/ " << global_str << " /" << local_int << global_int <<std::endl;
+//	std::string local_str;
+//	int local_int;
+//	std::cout << "/ " << local_str << " /" << "/ " << global_str << " /" << local_int << global_int <<std::endl;
+//	int double = 3.14; error: expected unqualified-id before "=" token
+//	int _; no error
+//	int catch-22; error
+//	int 1_or_2 = 1; error
+//	double Double = 3.14; no error
 	return 0;
 }
