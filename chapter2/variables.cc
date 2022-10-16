db34a9eb976c20d8ba6cbd414de601dd2250c070
santosh-ui <rajusantosh77@gmail.com>
2022-10-16 12:52:21 -0400

    adds variable example

---
+++ chapter2/variables.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+std::string global_str;
+int global_int;
+int main(){
+//	int input_value = 0;
+//	int i = 3.14; // no error double converted to int
+	// int i = {3.14}; error: narrowing conversion
+	// double salary = wage = 294.99; error: wage was not declared in this scope.
+//	std::cout << "Please, enter a value:" << std::endl;
+//	std::cin >> input_value;
+//	std::cout << "the input value is: " << input_value << "\ti value is: " << i << std::endl;
+	
+	std::string local_str;
+	int local_int;
+	std::cout << "/ " << local_str << " /" << "/ " << global_str << " /" << local_int << global_int <<std::endl;
+	return 0;
+}
