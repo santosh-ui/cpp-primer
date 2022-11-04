fcd117c6047cd55de8451ee5a153e0e8dffe3749
santosh-ui <rajusantosh77@gmail.com>
2022-11-04 06:19:52 -0400

    Sales_data header

--- chapter2/Sales_data.cc
+++ chapter2/Sales_data.cc
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<string>
+#include "Sales_data.h"
 /* 
  * struct Sales_data{
  * '/'* ...*'/'
@@ -8,9 +10,13 @@
 // equivalent, but better way to define the objects
 // struct Sales_data{}
 // error: expected semicolon after struct definition
-struct Sales_data{};
-Sales_data accum, trans, *salesptr;
+// struct Sales_data{};
+// Sales_data accum, trans, *salesptr;
 int main(){
+	Sales_data data1, data2;
+	// code to read into data1 and data2
+	// code to check whether data1 and data2 have the same ISBN
+	// and if so print the sum of data1 and data2
 	return 0;
 }
 
